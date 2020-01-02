5843932244abe3c87a12cb4bd24d367d6f3b88d2
Leon Bogaert <leon@tim-online.nl>
2020-01-02 11:05:18 +0100

    Betere timeout bij netwerk issues?

--- websocket.go
+++ websocket.go
@@ -30,6 +30,7 @@ var (
 const (
 	// Time allowed to write a message to the peer.
 	writeWait = 10 * time.Second
+	readWait  = 10 * time.Second
 
 	// Send pings to peer with this period. Must be less than pongWait.
 	pingPeriod = 60 * time.Second
@@ -155,6 +156,9 @@ func (ws *Websocket) Connect(ctx context.Context) error {
 	if err != nil {
 		return err
 	}
+
+	// set a timeout for receiving answers
+	ws.connection.SetReadDeadline(time.Now().Add(readWait))
 
 	// Send ping messages. Stop doing that when context is canceled
 	go func() {
