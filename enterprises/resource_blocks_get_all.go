37805876f05a63ddfb85a8502d66aca21bb023ee
Leon Bogaert <leon@tim-online.nl>
2021-01-11 11:34:36 +0100

    Added ResourceBlocks response type

--- enterprises/resource_blocks_get_all.go
+++ enterprises/resource_blocks_get_all.go
@@ -1,6 +1,8 @@
 package enterprises
 
 import (
+	"time"
+
 	"github.com/tim-online/go-mews/configuration"
 	"github.com/tim-online/go-mews/json"
 	"github.com/tim-online/go-mews/omitempty"
@@ -39,12 +41,12 @@ func (s *APIService) NewResourceBlocksGetAllRequest() *ResourceBlocksGetAllReque
 type ResourceBlocksGetAllRequest struct {
 	json.BaseRequest
 
-	ResourceBlockIDs    []string                   `json:"ResourceBlockIds"`    // Unique identifiers of the requested Resource blocks.
-	AssignedResourceIDs []string                   `json:"AssignedResourceIds"` // Unique identifiers of the requested Assigned Resources.
-	CollidingUTC        configuration.TimeInterval `json:"CollidingUtc"`        // Interval in which the Resource block is active.
-	CreatedUTC          configuration.TimeInterval `json:"CreatedUtc"`          // Interval in which the Resource block was created.
-	UpdatedUTC          configuration.TimeInterval `json:"UpdatedUtc"`          // Interval in which the Resource block was updated.
-	Extent              ResourceBlockExtent        `json:"Extent,omitempty"`    // Extent of data to be returned.
+	ResourceBlockIDs    []string                   `json:"ResourceBlockIds"`       // Unique identifiers of the requested Resource blocks.
+	AssignedResourceIDs []string                   `json:"AssignedResourceIds"`    // Unique identifiers of the requested Assigned Resources.
+	CollidingUTC        configuration.TimeInterval `json:"CollidingUtc,omitempty"` // Interval in which the Resource block is active.
+	CreatedUTC          configuration.TimeInterval `json:"CreatedUtc,omitempty"`   // Interval in which the Resource block was created.
+	UpdatedUTC          configuration.TimeInterval `json:"UpdatedUtc,omitempty"`   // Interval in which the Resource block was updated.
+	Extent              ResourceBlockExtent        `json:"Extent,omitempty"`       // Extent of data to be returned.
 }
 
 func (r ResourceBlocksGetAllRequest) MarshalJSON() ([]byte, error) {
@@ -52,8 +54,29 @@ func (r ResourceBlocksGetAllRequest) MarshalJSON() ([]byte, error) {
 }
 
 type ResourceBlocksGetAllResponse struct {
+	ResourceBlocks ResourceBlocks `json:"ResourceBlocks"`
+}
+
+type ResourceBlocks []ResourceBlock
+
+type ResourceBlock struct {
+	ID                 string            `json:"Id"`                 // Unique identifier of the block.
+	AssignedResourceID string            `json:"AssignedResourceId"` // Unique identifier of the assigned Resource.
+	IsActive           bool              `json:"IsActive"`           // Whether the block is still active.
+	Type               ResourceBlockType `json:"Type"`               // Type of the resource block.
+	StartUTC           time.Time         `json:"StartUtc"`           // Start of the block in UTC timezone in ISO 8601 format.
+	EndUTC             time.Time         `json:"EndUtc"`             // End of the block in UTC timezone in ISO 8601 format.
+	CreatedUTC         time.Time         `json:"CreatedUtc"`         // Creation date and time of the block in UTC timezone in ISO 8601 format.
+	UpdatedUTC         time.Time         `json:"UpdatedUtc"`         // Last update date and time of the block in UTC timezone in ISO 8601 format.
 }
 
 type ResourceBlockExtent struct {
 	Inactive bool `json:"Inactive"` // Whether the response should contain inactive entities.
 }
+
+type ResourceBlockType string
+
+const (
+	ResourceBlockTypeOutOfOrder  ResourceBlockType = "OutOfOrder"
+	ResourceBlockTypeInternalUse ResourceBlockType = "InternalUse"
+)
